4b7a1a6f650006fe9fc10540a53763dc80912755
shivam <jhodshivam5122@gmail.com>
2023-01-26 14:45:33 +0530

    list

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+
+
+export const shazamCoreApi = createApi({
+    reducerPath:'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl:'https://shazam.p.rapidapi.com',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '6c9f1863damsh61951e86ba9eeb0p158ae2jsn80a3dfb43fe0')
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({query:() => '/charts/track'}),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi
